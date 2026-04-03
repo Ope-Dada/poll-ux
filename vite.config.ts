3bbd340c72a59837911395b88c4105b25c6b5860
Ope-Dada <opeyemi.m.dada@gmail.com>
2026-04-03 20:09:30 +0100

    fix: remove terser, use default vite minifier

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     target: 'es2015',
-    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
